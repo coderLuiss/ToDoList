2d2795f43cc72cc59210bcf9c2c6abd64f45a787
Luis Menjivar <luismenjivar0325@gmail.com>
2024-12-22 23:07:36 -0500

    Added completing feature for task

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const addTaskButton = document.getElementById('addTaskButton');
 const cancelNewTaskButton = document.getElementById('closePopup');
 const submitNewTaskButton = document.getElementById('submitButton');
 const taskList = document.getElementById('taskList');
-
+const completedTaskButton = document.getElementById('complete-task-button');
 
 addTaskButton.addEventListener('click', () => {
     document.getElementById('newTaskFormContainer').style.display = 'flex';
@@ -29,22 +29,35 @@ function addNewTask(event) {
     
     const inputTaskDetails = document.getElementById('newTaskDetails');
     taskList.appendChild(newTask);
+    if(document.getElementById('newTaskPriority').value === 'high') {
+        inputTaskTitle.value = "!!!"  + inputTaskTitle.value;
+    } else if(document.getElementById('newTaskPriority').value === 'medium') {
+        inputTaskTitle.value = "!!"  + inputTaskTitle.value;
+    } else {
+        inputTaskTitle.value = "!"  + inputTaskTitle.value;
+    }
     newTask.innerHTML = `
     <p>
     <span class="task-title">${inputTaskTitle.value}</span><br>
     <span class="task-details">${inputTaskDetails.value}</span>
     </p>
-    <button class="complete-task-button"></button>`;    
-    if(document.getElementById('newTaskPriority').value === 'high') {
-        newTask.style.backgroundColor = 'rgb(236, 100, 75)';
-    } else if(document.getElementById('newTaskPriority').value === 'medium') {
-        newTask.style.backgroundColor = 'rgb(248, 119, 14)';
-    } else {
-        newTask.style.backgroundColor = 'rgb(237, 219, 60)';
+    <button id="complete-task-button"></button>`;    
+    
+    const completedTaskButton = newTask.querySelector('#complete-task-button');
+    if (completedTaskButton) {
+        completedTaskButton.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = "rgb(125, 165, 252)";
+        event.target.style.boxShadow = "inset 0 0 5px rgba(0, 0, 0, 0.5)";
+        event.target.style.padding = "5px";
+        setTimeout(() => {
+            taskList.remove(newTask);
+        }, 2000);
+    });
     }
     document.getElementById('newTaskFormContainer').style.display = 'none';
     inputTaskTitle.value = '';
     inputTaskDetails.value = '';
 }
+
 
 
